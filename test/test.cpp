63ded55efa67e4f9de4b4a22849c797874b07a56
Wes <wjacademic@gmail.com>
2018-07-31 20:40:43 -0700

    Added file to test c++

---
+++ test/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<string> v;
+    v.push_back("hello world");
+    cout << v.size() << endl;
+    return 0;
+}
